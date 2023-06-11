1f75ce14f723f84b852e8dde59e643dfd4bbfcdd
gustaveWPM <jgeorge@perso.be>
2023-06-11 16:53:17 +0200

    add: example 2.4 + some syntax fixes

--- examples/example2.hs
+++ examples/example2.hs
@@ -1,22 +1,22 @@
--- Example 2.1 — The anagram checker
-
 import Data.List
+import Data.Char
+import Data.List
+
+-- Example 2.1 — The anagram checker
 
 isAnagram :: String -> String -> Bool
 isAnagram word1 word2 = (sort word1) == (sort word2)
 
 -- Example 2.2 — The word validator
 
-import Data.Char
-import Data.List
-
 isWord :: String -> Maybe String
-case (null word) of
-  True -> Nothing
-  False ->
-    case (all isAlpha word) of
-      False -> Nothing
-      True -> Just word
+isWord word =
+  case (null word) of
+    True -> Nothing
+    False ->
+      case (all isAlpha word) of
+        False -> Nothing
+        True -> Just word
 
 -- Example 2.3 — Validate first, then compare
 
@@ -31,3 +31,12 @@ checkAnagram word1 word2 =
           case (isAnagram word1 word2) of
             False -> "These words are not anagrams."
             True -> "These words are anagrams."
+
+-- Example 2.4 — Interactive program
+
+main :: IO ()
+main =
+  do
+    word1 <- getLine
+    word2 <- getLine
+    print (checkAnagram word1 word2)
